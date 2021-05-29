e8aa7e1232bf41590e7c943eb7177bcf38bf37a1
Peter Trevan <kirner10@gmail.com>
2021-05-29 17:11:59 +0100

    Add model

--- jam/main.go
+++ jam/main.go
@@ -1,15 +1,24 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	fmt.Println("Starting Server...")
+	var err error
+	gameStateJson, err := ioutil.ReadFile("static/state_default.json")
+	var gameState GameState
+	json.Unmarshal([]byte(gameStateJson), &gameState)
+	if err != nil {
+		fmt.Println("File reading error", err)
+		return
+	}
 
-	gin.SetMode(gin.DebugMode)
+	fmt.Println("Starting Server...")
 
 	router := gin.Default()
 	router.StaticFile("/", "./static/home.html")
--- jam/model.go
+++ jam/model.go
@@ -6,17 +6,57 @@ import (
 )
 
 var tables map[uuid.UUID]Table = make(map[uuid.UUID]Table)
+var gameStateJson string
 
 type Game struct {
 	Value int
 	Tag   string `json:"label"`
 }
 
-type GameState struct {
-}
-
 type Table struct {
 	Id   uuid.UUID
 	Name string
 	Conn *websocket.Conn
+}
+
+type GameState struct {
+	PlayerTurn bool         `json:"playerTurn"`
+	Player     Player       `json:"player"`
+	Opponent   Opponent     `json:"opponent"`
+	BuyArea    []SupplyPile `json:"buyArea"`
+}
+
+type Player struct {
+	Hand    []Card `json:"hand"`
+	Desk    int    `json:"deck"`
+	Discard int    `json:"discard"`
+	Score   int    `json:"score"`
+}
+
+type Opponent struct {
+	Hand    int `json:"hand"`
+	Deck    int `json:"deck"`
+	Discard int `json:"discard"`
+	Score   int `json:"score"`
+}
+
+type SupplyPile struct {
+	Amount int  `json:"amount"`
+	Card   Card `json:"card"`
+}
+
+type Card struct {
+	Id          int     `json:"id"`
+	Name        string  `json:"name"`
+	Image       string  `json:"image"`
+	Description string  `json:"description"`
+	Blue        bool    `json:"blue"`
+	Special     bool    `json:"special"`
+	Effects     Effects `json:"effects"`
+}
+
+type Effects struct {
+	Attack  int `json:"attack"`
+	Actions int `json:"actions"`
+	Cards   int `json:"cards"`
 }
